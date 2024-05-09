8b2d8e45f232ab33d6e47a078ddad7301a6d78ed
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-05-09 11:57:03 +0100

    added padding option to text/full image

--- page-templates/blocks/cb_text_image.php
+++ page-templates/blocks/cb_text_image.php
@@ -5,7 +5,14 @@ $image_order = get_field('order') == 'Image/Text' ? '' : 'order-md-2';
 $text_order = get_field('order') == 'Image/Text' ? '' : 'order-md-1';
 $text_alignment = get_field('order') == 'Image/Text' ? 'text--right px-3' : 'text--left px-3';
 
-$img = wp_get_attachment_image_url(get_field('image'),'full') ?: get_stylesheet_directory_uri() . '/img/placeholder-800x450.png';
+$img = wp_get_attachment_image_url(get_field('image'), 'full') ?: get_stylesheet_directory_uri() . '/img/placeholder-800x450.png';
+
+$padding = get_field('padding');
+if (!is_array($padding)) {
+    $padding = [];  // Assign an empty array if not an array
+}
+$pt = in_array("top", $padding) ? 'pt-5' : null;
+$pb = in_array("bottom", $padding) ? 'pb-5' : null;
 ?>
 <section class="text_image <?=$bg?>">
     <div class="container-fluid">
@@ -13,7 +20,8 @@ $img = wp_get_attachment_image_url(get_field('image'),'full') ?: get_stylesheet_
             <div class="col-md-6 text_image__image <?=$image_order?>"
                 style="background-image:url(<?=$img?>)">
             </div>
-            <div class="col-md-6 <?=$text_order?>">
+            <div
+                class="col-md-6 <?=$text_order?> <?=$pt?> <?=$pb?>">
                 <div class="ps-xl-3 <?=$text_alignment?>">
                     <h2 class="mb-4">
                         <?=get_field('title')?>

fdfc46b3dc40e19f13edc11373fb1f72293e6de8
ChillibyteUK <dan@chillibyte.co.uk>
2025-08-21 14:53:57 +0100

    archives

--- archive.php
+++ archive.php
@@ -40,8 +40,10 @@ if ( ! empty( $queried_object ) && isset( $queried_object->slug ) ) {
 					if ( 'uncategorized' === strtolower( $category->slug ) ) {
 						continue;
 					}
+                    $is_active = ( isset( $category_slug ) && $category_slug === $category->slug ) ? ' active' : '';
                     printf(
-                        '<a class="category-item" href="%s">%s</a>',
+                        '<a class="category-item%s" href="%s">%s</a>',
+                        esc_attr( $is_active ),
                         esc_url( get_category_link( $category->term_id ) ),
                         esc_html( $category->name )
                     );
@@ -55,47 +57,47 @@ if ( ! empty( $queried_object ) && isset( $queried_object->slug ) ) {
     <section class="latest pb-5 mb-5">
         <div class="container-xl">
             <div class="section_title">
-                <h2 class="fs-800"><?= single_cat_title() ?> articles</h2>
+                <h2 class="fs-800"><?= esc_html( single_cat_title() ); ?> articles</h2>
             </div>
             <div class="latest_grid">
                 <?php
-                if (have_posts()) {
+                if ( have_posts() ) {
                     $c = 1;
-                    while (have_posts()) {
+                    while ( have_posts() ) {
                         the_post();
-                        $ph = get_the_post_thumbnail_url(get_the_ID(), 'large') ?: get_stylesheet_directory_uri() . '/img/placeholder-800x450.png';
-                        $cat = get_the_category();
-                ?>
+                        $ph              = get_the_post_thumbnail_url( get_the_ID(), 'large' ) ? get_the_post_thumbnail_url( get_the_ID(), 'large' ) : get_stylesheet_directory_uri() . '/img/placeholder-800x450.png';
+                        $post_categories = get_the_category();
+                		?>
                         <a class="grid__card"
-                            href="<?= get_the_permalink() ?>">
-                            <img class="card__image" src="<?= $ph ?>">
+                            href="<?= esc_url( get_the_permalink() ); ?>">
+                            <img class="card__image" src="<?= esc_url( $ph ); ?>">
                             <div class="card__inner">
                                 <div
-                                    class="card__category cat--<?= $cat[0]->slug ?>">
+                                    class="card__category cat--<?= esc_attr( $post_categories[0]->slug ); ?>">
                                 </div>
-                                <div class="card__title"><?= get_the_title() ?>
+                                <div class="card__title"><?= esc_html( get_the_title() ); ?>
                                     <?php
-                                    if ($c == 1) {
-                                    ?>
+                                    if ( 1 === $c ) {
+                                    	?>
                                         <div class="card__excerpt d-md-none">
-                                            <?= wp_trim_words(get_the_content(null, false, get_the_ID()), 20) ?>
+                                            <?= wp_kses_post( wp_trim_words( get_the_content( null, false, get_the_ID() ), 20 ) ); ?>
                                         </div>
                                         <div class="card__excerpt d-none d-md-block">
-                                            <?= wp_trim_words(get_the_content(null, false, get_the_ID()), 50) ?>
+                                            <?= wp_kses_post( wp_trim_words( get_the_content( null, false, get_the_ID() ), 50 ) ); ?>
                                         </div>
-                                    <?php
+                                    	<?php
                                     }
                                     ?>
                                 </div>
                                 <div class="card__meta">
                                     <div class="card__meta_date">
-                                        <?= get_the_date('j M, Y') ?>
+                                        <?= esc_html( get_the_date( 'j M, Y' ) ); ?>
                                     </div>
                                     <div class="card__meta_time">
-                                        <?= estimate_reading_time_in_minutes(get_the_content(null, false, get_the_ID()), 200, true, true) ?>
+                                        <?= wp_kses_post( estimate_reading_time_in_minutes( get_the_content( null, false, get_the_ID() ), 200, true, true ) ); ?>
                                     </div>
                                     <div
-                                        class="card__meta_link link--<?= $cat[0]->slug ?>">
+                                        class="card__meta_link link--<?= esc_attr( $post_categories[0]->slug ); ?>">
                                         <span class="fa-stack fa-2x">
                                             <i class="fa-solid fa-circle fa-stack-2x"></i>
                                             <i class="fa-solid fa-arrow-right-long fa-stack-1x fa-inverse"></i>
@@ -104,8 +106,8 @@ if ( ! empty( $queried_object ) && isset( $queried_object->slug ) ) {
                                 </div>
                             </div>
                         </a>
-                <?php
-                        $c++;
+                		<?php
+                        ++$c;
                     }
                 }
                 ?>
